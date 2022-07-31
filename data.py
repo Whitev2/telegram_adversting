890c054beaa925f44557cc5b7f5feab568a04767
White <WilliamHarris9584680@gmail.com>
2022-07-31 18:32:29 +0300

    data

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import os
-
-import psycopg2
+import motor.motor_asyncio
 from aiogram import Bot
 
 
@@ -12,16 +11,13 @@ class Data:
         self.mg_pswd = os.getenv('MONGO_PASSWORD')
         self.mg_host = os.getenv('MONGO_HOST')
         self.mg_port = os.getenv('MONGO_PORT')
-        self.super_admins = []
+        self.admins = []
         self.THROTTLE_TIME = 0.8
-        self.commichannel = os.getenv('COMMIT_CH')
-        self.masterchannel = os.getenv('MASTER_CH')
-# фывфывфдв
+
     def get_bot(self):
         return Bot(self.bot_token, parse_mode="HTML")
 
-    def get_postg(self):
-        return psycopg2.connect(database=self.pg_db, user=self.pg_user, password=self.pg_pswd, host=self.pg_host, port=int(self.pg_port))
-
-    def get_mongo(self):
-        return motor.motor_asyncio.AsyncIOMotorClient(username=self.mg_user, password=self.mg_pswd, host=self.mg_host, port=int(self.mg_port))
+    def mongo_data(self):
+        client = motor.motor_asyncio.AsyncIOMotorClient(username=self.mg_user, password=self.mg_pswd, host=self.mg_host,
+                                                        port=int(self.mg_port))
+        return client.database
