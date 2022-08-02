56a7bbf896ca56644a8dd419ad24327f3906d25e
White <WilliamHarris9584680@gmail.com>
2022-08-02 21:22:05 +0300

    order

--- handlers/customer_menu_hand.py
+++ handlers/customer_menu_hand.py
@@ -34,4 +34,61 @@ async def info(message: Message, state: FSMContext):
 
 @router.callback_query(lambda call: 'customer' in call.data)
 async def add_telegram(query: types.CallbackQuery, state: FSMContext):
-    await query.message.answer(str(query.data.split()))
+    await query.message.delete()
+    await state.set_state(Customer.start_answers)
+    callback = query.data.split()
+    await state.update_data(callback=callback)
+    ch_name = str()
+    if callback[-1] == 'channel':
+        await query.message.answer(f'Пришлите мне ссылку на канал, на который нужно подписаться')
+    elif callback[-1] == 'bot':
+        await query.message.answer(f'Пришлите мне ссылку на бота, которого необходимо пройти')
+    elif callback[-1] == 'post':
+        await query.message.answer(f'Пришлите мне ссылку на пост, который необходимо посмотреть')
+    elif callback[-1] == 'reaction':
+        await query.message.answer(f'Пришлите мне ссылку на пост, где необходимо оставить реакцию')
+
+
+@router.message(state=Customer.start_answers)
+async def start_answers(message: Message, state: FSMContext):
+    # проверка есть ли исчточник в списке заданий в процессе или нет
+
+    if '@' in message.text or 'http' in message.text: # проверка на формат
+        await state.set_state()
+        await state.update_data(answer_1=message.text)
+        await state.set_state(Customer.answer_2)
+        await message.answer('Напишите стоимость одного выполнения')
+    else:
+        await message.answer("Ошибка, введенная ссылка не действительна, повторите попытку")
+
+
+@router.message(state=Customer.answer_2)
+async def start_answers(message: Message, state: FSMContext):
+    if float(message.text):
+        await state.set_state(Customer.answer_3)
+        await state.update_data(answer_2=message.text)
+        await message.answer('Напишите колличество выполниний')
+    else:
+        await message.answer("Простите, я принимаю только числа")
+
+
+@router.message(state=Customer.answer_3)
+async def start_answers(message: Message, state: FSMContext):
+    data = await state.get_data()
+    channel_name = data['answer_1']
+    click_price = data['answer_2']
+    summ = int(message.text)*float(click_price)
+    if int(message.text):  # проверка на число
+        markup = ReplyKeyboardBuilder()
+        markup.row(types.KeyboardButton(text='Отменить'))
+        markup.row(types.KeyboardButton(text='Подтвердить'))
+        markup.adjust(2)
+        await state.update_data(answer_2=message.text)
+        await message.answer(f'Вы хотите заказать рекламу для {channel_name}\n\n'
+                             f'Колличество человек: {message.text}\n'
+                             f'Стоимость выполнения: {click_price}\n\n'
+                             f'Сумма заказа: {summ}\n'
+                             f'Комиссия 2% от суммы: {summ/100*2}\n\n'
+                             f'Итого к оплате: {summ+(summ/100*2)}', reply_markup=markup.as_markup(resize_keyboar=True))
+    else:
+        await message.answer("Простите, я принимаю только числа")
--- states/customer_states.py
+++ states/customer_states.py
@@ -3,3 +3,6 @@ from aiogram.dispatcher.fsm.state import State, StatesGroup
 
 class Customer(StatesGroup):
     customer_menu = State()
+    start_answers = State()
+    answer_2 = State()
+    answer_3 = State()
